5f824816b2005cd14dc7145a67e3c572700f5f41
choumasamori <5911378@gmail.com>
2018-03-23 11:00:01 +0700

    showHide

--- src/views/TableList/TableList.jsx
+++ src/views/TableList/TableList.jsx
@@ -3,64 +3,83 @@ import { Grid } from "material-ui";
 
 import { RegularCard, Table, ItemGrid } from "components";
 
-function TableList({ ...props }) {
-  return (
-    <Grid container>
-      <ItemGrid xs={12} sm={12} md={12}>
-        <RegularCard
-          cardTitle="Simple Table"
-          cardSubtitle="Here is a subtitle for this table"
-          content={
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["Name", "Country", "City", "Salary"]}
-              tableData={[
-                ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
-                ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
-                ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
-                ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
-                ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
-                ["Mason Porter", "Chile", "Gloucester", "$78,615"]
-              ]}
-            />
-          }
-        />
-      </ItemGrid>
-      <ItemGrid xs={12} sm={12} md={12}>
-        <RegularCard
-          plainCard
-          cardTitle="Table on Plain Background"
-          cardSubtitle="Here is a subtitle for this table"
-          content={
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["ID", "Name", "Country", "City", "Salary"]}
-              tableData={[
-                ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
-                ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
-                ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
-                [
-                  "4",
-                  "Philip Chaney",
-                  "$38,735",
-                  "Korea, South",
-                  "Overland Park"
-                ],
-                [
-                  "5",
-                  "Doris Greene",
-                  "$63,542",
-                  "Malawi",
-                  "Feldkirchen in Kärnten"
-                ],
-                ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
-              ]}
-            />
-          }
-        />
-      </ItemGrid>
-    </Grid>
-  );
+export class TableList extends React.Component{
+  
+  showDetails(id) {
+    if ( !document.getElementById(id).classList.contains('visible')  ) {
+			document.getElementById(id).setAttribute('class', 'visible');
+			document.getElementById(id).style.display = "block";
+		}
+		else {
+			document.getElementById(id).className = '';
+			document.getElementById(id).style.display = "none";
+		}
+    
+	}
+  render(){
+    return (
+      <Grid container>
+        <button onClick={()=>this.showDetails("table1")} type="submit">Show/Hide</button>
+          <ItemGrid xs={12} sm={12} md={12}>
+          <div id="table1">
+          <RegularCard
+            cardTitle="Simple Table"
+            cardSubtitle="Here is a subtitle for this table"
+            content={
+              <Table
+                tableHeaderColor="primary"
+                tableHead={["Name", "Country", "City", "Salary"]}
+                tableData={[
+                  ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"],
+                  ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"],
+                  ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"],
+                  ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"],
+                  ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"],
+                  ["Mason Porter", "Chile", "Gloucester", "$78,615"]
+                ]}
+              />
+            }
+          />
+          </div>
+        </ItemGrid>
+        <button onClick={()=>this.showDetails("table2")} type="submit">Show/Hide</button>
+        <ItemGrid xs={12} sm={12} md={12}>
+          <div id="table2">
+          <RegularCard
+            plainCard
+            cardTitle="Table on Plain Background"
+            cardSubtitle="Here is a subtitle for this table"
+            content={
+              <Table
+                tableHeaderColor="primary"
+                tableHead={["ID", "Name", "Country", "City", "Salary"]}
+                tableData={[
+                  ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
+                  [
+                    "4",
+                    "Philip Chaney",
+                    "$38,735",
+                    "Korea, South",
+                    "Overland Park"
+                  ],
+                  [
+                    "5",
+                    "Doris Greene",
+                    "$63,542",
+                    "Malawi",
+                    "Feldkirchen in Kärnten"
+                  ],
+                  ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
+                ]}
+              />
+            }
+          />
+          </div>
+        </ItemGrid>
+      </Grid>
+    );
+  }
 }
-
 export default TableList;
